625f514ba1c63e44c9c2f5ae1fdb1e419386e941
sly <1311220942@qq.com>
2019-06-08 20:36:32 +0800

    提交

---
+++ fescar-dubbo-business/src/main/java/com/sly/fescar/business/FescarDubboBusinessApplication.java
@@ -0,0 +1,13 @@
+package com.sly.fescar.business;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FescarDubboBusinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FescarDubboBusinessApplication.class, args);
+	}
+
+}
